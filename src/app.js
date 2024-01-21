9d7eccd180aecf0a7c01b1a9a5c9452b80ebb1cc
mrinalsinha2211 <mrinalsinha53@gmail.com>
2024-01-21 22:51:23 +0530

    changed

--- src/app.js
+++ src/app.js
@@ -15,7 +15,50 @@ app.use(express.json());
 // Write PATCH endpoint to buy a product for the client here
 // Endpoint /api/v1/products/:id
 
+app.patch("/api/v1/products/:id" , async(req,res)=>{
+    const productId=parseInt(req.params.id);
+    try {
+        const productsData = await fs.readFile(`${__dirname}/data/product.json`, 'utf-8');
+        const products = JSON.parse(productsData);
+    
+        const productIndex=products.findIndex((product)=>{
+            product.id===productId
+        });
+        if(productIndex!==-1){
+            if(products[productIndex].quantity>0){
+                products[productIndex].quantity--;
+                //   await fs.writeFile(`${__dirname}/data/product.json`,JSON.stringify(products, null, 2));
+                await fs.writeFile(productsFilePath, JSON.stringify(products, null, 2));
 
+                res.status(200).json({
+                    status: 'success',
+                    message: `Thank you for purchasing ${products[productIndex].name}`,
+                    product: {
+                      id: products[productIndex].id,
+                      name: products[productIndex].name,
+                      price: products[productIndex].price,
+                      quantity: products[productIndex].quantity,
+                    },
+                  });
+            }else{
+                res.status(404).json({
+                    status: 'success',
+                    message: `${products[productIndex].name}, Out of stock!`,
+                  });
+            }
+        } else {
+            res.status(404).json({
+              status: 'failed',
+              message: 'Product not found!',
+            });
+          }
+    } catch (error) {
+        res.status(500).json({
+            status: 'error',
+            message: 'Internal server error',
+          });
+    }
+});
 
 
 module.exports = app;
